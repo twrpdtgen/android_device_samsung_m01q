a1d8bffde8ea6c1c6a6130cb34fedce2ed65dc6a
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-10-22 07:03:39 +0200

    m01q: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: 0 Manufacturer: samsung Model: SM-M015G Script version: 2.0.1 Signed-off-by: Sebastiano Barezzi <barezzisebastiano@gmail.com>

---
+++ omni_m01q.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from m01q device
+$(call inherit-product, device/samsung/m01q/device.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := m01q
+PRODUCT_NAME := omni_m01q
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-M015G
+PRODUCT_MANUFACTURER := samsung
